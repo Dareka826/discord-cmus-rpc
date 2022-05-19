6d3f73285f15568e735c21153aaa15837ea34bfc
Rin Takanashi <dareka826@gmail.com>
2022-05-19 17:45:55 +0200

    parse cmus-remote output

--- main.c
+++ main.c
@@ -5,6 +5,12 @@
 #include <time.h>
 #include <unistd.h>
 
+enum {
+    PIPE_READ = 0,
+    PIPE_WRITE
+};
+
+// DISCORD {{{
 const char * const APP_ID = "976582643938377749";
 
 // Functions to notify of events {{{
@@ -51,13 +57,180 @@ void discord_update_presence() { /*{{{*/
 
     Discord_UpdatePresence(&presence);
 } /*}}}*/
+// }}}
+
+// CMUS {{{
+struct cmus_state {
+    int status; // 1 is playing
+    unsigned int position;
+    unsigned int duration;
+
+    char *title;
+    char *file;
+    char *artist;
+    int tracknumber;
+
+    char *album;
+    char *albumartist;
+};
+
+/*
+status paused
+file /home/rin/Music/[SPACELECTRO] VOCALO EDM10 HEAVY/01. Spacelectro - Reincarnation feat. Hatsune Miku.opus
+duration 298
+position 0
+tag album VOCALO EDM10 HEAVY
+tag albumartist SPACELECTRO
+tag artist Spacelectro
+tag title Reincarnation feat. Hatsune Miku
+tag tracknumber 1
+*/
+void cmus_get_metadata(struct cmus_state *c) { /*{{{*/
+    // Spawn cmus-remote and parse its output
+    int pipefd[2];
+    pipe(pipefd);
+
+    pid_t pid = fork();
+    if(pid == -1) exit(EXIT_FAILURE);
+
+    if(pid == 0) {
+        // Child
+        close(pipefd[PIPE_READ]);
+        dup2(STDOUT_FILENO, STDERR_FILENO); // Redirect stderr to stdout
+        dup2(pipefd[PIPE_WRITE], STDOUT_FILENO); // Redirect stdout to pipe
+
+        char *args[] = {"cmus-remote", "-Q", NULL};
+        execvp("cmus-remote", args);
+
+    } else {
+        // Parent
+        close(pipefd[PIPE_WRITE]);
+        FILE *child = fdopen(pipefd[PIPE_READ], "r");
+
+        char *line = NULL; // Line string
+        size_t len = 0;
+        size_t read_len = 0; // Length of read-in line
+
+        struct cmus_state cs;
+        memset(&cs, 0, sizeof(cs));
+        cs.tracknumber = -1;
+
+        while((read_len = getline(&line, &len, child)) != -1) {
+
+            // Get rid of newline
+            if(line[read_len-1] == '\n')
+                line[read_len-1] = '\0';
+
+            // Check if cmus is running
+            if(strncmp(line, "cmus-remote: cmus is not running", 32) == 0) {
+                cs.status = 0;
+                break;
+            }
+
+            // Tags
+            else if(strncmp(line, "tag ", 4) == 0) {
+                // Title
+                if(strncmp(line+4, "title ", 6) == 0) {
+                    int len = strnlen(line+10, 4096) + 1;
+                    cs.title = (char*) malloc(len);
+                    memset(cs.title, 0, len);
+
+                    strncpy(cs.title, line+10, len);
+                }
+
+                // Artist
+                else if(strncmp(line+4, "artist ", 7) == 0) {
+                    int len = strnlen(line+11, 4096) + 1;
+                    cs.artist = (char*) malloc(len);
+                    memset(cs.artist, 0, len);
+
+                    strncpy(cs.artist, line+11, len);
+                }
+
+                // Track
+                else if(strncmp(line+4, "tracknumber ", 12) == 0) {
+                    long num = strtol(line+16, NULL, 10);
+                    cs.tracknumber = num;
+                }
+
+                // Album
+                else if(strncmp(line+4, "album ", 6) == 0) {
+                    int len = strnlen(line+10, 4096) + 1;
+                    cs.album = (char*) malloc(len);
+                    memset(cs.album, 0, len);
+
+                    strncpy(cs.album, line+10, len);
+                }
+
+                // Album Artist
+                else if(strncmp(line+4, "albumartist ", 12) == 0) {
+                    int len = strnlen(line+16, 4096) + 1;
+                    cs.albumartist = (char*) malloc(len);
+                    memset(cs.albumartist, 0, len);
+
+                    strncpy(cs.albumartist, line+16, len);
+                }
+            }
+
+            // Are we playing?
+            else if(strncmp(line, "status ", 7) == 0) {
+                if(strncmp(line+7, "playing", 7) == 0)
+                     cs.status = 1;
+                else cs.status = 0;
+            }
+
+            // Where we are in the track
+            else if(strncmp(line, "position ", 9) == 0) {
+                long num = strtol(line+9, NULL, 10);
+                cs.position = num;
+            }
+
+            // Track length
+            else if(strncmp(line, "duration ", 9) == 0) {
+                long num = strtol(line+9, NULL, 10);
+                cs.duration = num;
+            }
+
+            // Which file is being played
+            else if(strncmp(line, "file ", 5) == 0) {
+                int len = strnlen(line+5, 4096) + 1;
+                cs.file = (char*) malloc(len);
+                memset(cs.file, 0, len);
+
+                strncpy(cs.file, line+5, len);
+            }
+
+            // Free memory allocated by getline()
+            free(line); line = NULL;
+        }
+
+        // Copy gathered data into giver struct pointer
+        // TODO: work on the pointer without a local copy
+        memcpy(c, &cs, sizeof(cs));
+    }
+} /*}}}*/
+
+// }}}
 
 int main() {
-    discord_init();
+    //discord_init();
 
-    discord_update_presence();
-    sleep(10);
+    struct cmus_state cs;
+    cmus_get_metadata(&cs);
 
-    discord_shutdown();
+    printf("status: %d\n", cs.status);
+    printf("position: %d\n", cs.position);
+    printf("duration: %d\n", cs.duration);
+    printf("title: %s\n", cs.title);
+    printf("file: %s\n", cs.file);
+    printf("artist: %s\n", cs.artist);
+    printf("track: %d\n", cs.tracknumber);
+    printf("album: %s\n", cs.album);
+    printf("album_artist: %s\n", cs.albumartist);
+
+    //discord_update_presence();
+    //sleep(10);
+
+    //discord_shutdown();
     return EXIT_SUCCESS;
 }
