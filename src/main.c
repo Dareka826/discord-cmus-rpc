c5dbb6e0b93db35051ec34ebfbaa4558969f510e
Rin Takanashi <dareka826@gmail.com>
2022-05-21 17:47:56 +0200

    Clean up main.c a bit

--- src/main.c
+++ src/main.c
@@ -10,12 +10,7 @@
 
 int main() {
     // TODO:
-    // - change status on pause/play
-    // - change song if metadata changes
     // - free stuff on SIGTERM
-
-    // - if status != "playing", stop displaying rpc info
-    // - if song data suddenly changes (check every 1 sec), reset rpc data
 
     discord_init();
 
@@ -23,15 +18,16 @@ int main() {
     struct presence_state ps, old_ps;
 
     int old_status = 0;
+
     memset(&old_ps, 0, sizeof(old_ps));
-    old_ps.state = (char*) malloc(1);
+    old_ps.state = (char*) xmalloc(1);
     old_ps.state[0] = '\0';
-    old_ps.details = (char*) malloc(1);
+    old_ps.details = (char*) xmalloc(1);
     old_ps.details[0] = '\0';
 
     // Main logic {{{
     while(1) {
-        if(MEM_INFO_D) fprintf(stderr, "\n");
+        if(MEM_INFO_D) fprintf(stderr, "\n"); // Better visibility
 
         // Clear out variables
         memset(&cs, 0, sizeof(cs));
@@ -39,28 +35,30 @@ int main() {
 
         cmus_get_metadata(&cs);
 
+        // cmus is playing
         if(cs.status == 1) {
             create_status(&cs, &ps);
 
             int changed = 0;
+            // Check if status changed from last time {{{
             {
-                int old_state_len = strnlen(old_ps.state, 512);
-                int new_state_len = strnlen(    ps.state, 512);
+                int old_state_len   = strnlen(old_ps.state,   512);
                 int old_details_len = strnlen(old_ps.details, 512);
+                int new_state_len   = strnlen(    ps.state,   512);
                 int new_details_len = strnlen(    ps.details, 512);
 
-                if( (old_status == 0) ||
-                    (old_state_len   != new_state_len)   ||
-                    (old_details_len != new_details_len) ||
-                    (strncmp(old_ps.state,   ps.state,   MAX(old_state_len,   new_state_len))   != 0) ||
-                    (strncmp(old_ps.details, ps.details, MAX(old_details_len, new_details_len)) != 0) ) {
+                if( (old_status == 0) || // Started playing
+                    (old_state_len   != new_state_len)   || // Length doesn't match
+                    (old_details_len != new_details_len) || // Length doesn't match
+                    (strncmp(old_ps.state,   ps.state,   MAX(old_state_len,   new_state_len))   != 0) ||  // Strings don't match
+                    (strncmp(old_ps.details, ps.details, MAX(old_details_len, new_details_len)) != 0) ) { // Strings don't match
 
                     changed = 1;
                 }
-            }
+            } /*}}}*/
 
             if(changed != 0) {
-                printf("[I]: Changed!\n");
+                printf("[I]: Status changed!\n");
 
                 printf("State: %s\n", ps.state);
                 printf("Deets: %s\n", ps.details);
@@ -85,7 +83,8 @@ int main() {
         }
 
         free_cmus_state(&cs);
-        if(MEM_INFO_D) fprintf(stderr, "\n");
+
+        if(MEM_INFO_D) fprintf(stderr, "\n"); // Better visibility
 
         // Prevent messages from piling-up
         fflush(stderr);
