a3b621db1030be486355696e68a3779930af3a91
Rin Takanashi <dareka826@gmail.com>
2022-05-21 18:05:41 +0200

    Reorder stuff a bit in tests for discord_stuff

--- tests/discord_stuff_test.c
+++ tests/discord_stuff_test.c
@@ -15,18 +15,19 @@ int main() {
     assert(ps.details   == NULL);
     assert(ps.time_left == 0);
 
-    const char * str = "Test1";
-    ps.state = (char*) xmalloc(strlen(str)+1);
-    memcpy(ps.state, str, strlen(str)+1);
-
+    const char * str1 = "Test1";
     const char * str2 = "Another string!";
+
+    ps.state   = (char*) xmalloc(strlen(str1)+1);
     ps.details = (char*) xmalloc(strlen(str2)+1);
+
+    memcpy(ps.state,   str1, strlen(str1)+1);
     memcpy(ps.details, str2, strlen(str2)+1);
 
     assert(ps.state   != NULL);
     assert(ps.details != NULL);
 
-    assert(strcmp(ps.state,   str ) == 0);
+    assert(strcmp(ps.state,   str1) == 0);
     assert(strcmp(ps.details, str2) == 0);
 
     // Freeing
