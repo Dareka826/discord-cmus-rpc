b5ccafc507a7ea55cf7cb04870a403ed810b7237
Rin Takanashi <dareka826@gmail.com>
2022-05-21 18:21:40 +0200

    Don't show extra memory information by default

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 CC="clang"
 CXX="clang++"
 CFLAGS="-g -O2 -Wall"
-CFLAGS="${CFLAGS} -DMEM_INFO_D=1" # For extra memory information
+#CFLAGS="${CFLAGS} -DMEM_INFO_D=1" # For extra memory information
 LDFLAGS=""
