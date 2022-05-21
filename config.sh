34382e22a6b0a4f7c72203e97513c0e9dcc8bfbe
Rin Takanashi <dareka826@gmail.com>
2022-05-21 13:51:06 +0200

    Remove C++ string warnings that don't apply to C code.

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
 CC="clang++"
-CFLAGS="-g -O2 -Wall -xc++"
+CFLAGS="-g -O2 -Wall"
+CFLAGS="-xc++ -Wno-writable-strings" # Have to compile as C++, because discord-rpc is
 CFLAGS="${CFLAGS} -DMEM_INFO_D=1" # For extra memory information
 LDFLAGS="-L./discord-rpc/builds/linux-static/src -ldiscord-rpc"
